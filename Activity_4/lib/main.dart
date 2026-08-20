7f74ceb64abbb93075a047988afc97d98a5c514a
Alyssa Rhenoa Nicole Bautista <>
2026-08-20 10:57:57 +0800

    code checkpoint

--- Activity_4/lib/main.dart
+++ Activity_4/lib/main.dart
@@ -13,3 +13,47 @@
 // - Presses the button.
 // - Close and Rereun the application. Value must persist.
 // - Uninstall and reinstall the application. Value should be empty again.
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Activity 3',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color(0xFF007236),
+          brightness: Brightness.dark,
+          dynamicSchemeVariant: DynamicSchemeVariant.fidelity,
+        ),
+        fontFamily: 'Roboto',
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const TestScreen(),
+    );
+  }
+}
+
+// ============================================================
+// TESTING SCREEN
+// ============================================================
+class TestScreen extends StatefulWidget {
+  const TestScreen({super.key});
+
+  @override
+  State<TestScreen> createState() => _TestScreenState();
+}
+
+class _TestScreenState extends State<TestScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+}
